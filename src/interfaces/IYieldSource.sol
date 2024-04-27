e8de2a9a6aefc196c150160f2b7caa4340338f0d
ortutay <marcell.ortutay@gmail.com>
2024-04-26 18:01:51 -0700

    rm console.log()

--- src/interfaces/IYieldSource.sol
+++ src/interfaces/IYieldSource.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/console.sol";
-
 interface IYieldSource {
 
     function asset() external view returns (address);
